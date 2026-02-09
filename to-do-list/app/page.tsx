5f04acbac0b1f4bce0585c528b3f1177872e667e
ShruthiH-2004 <shruthihiremath17@gmail.com>
2026-02-09 12:19:24 +0530

    Initial commit of To-Do List App

---
+++ to-do-list/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-4">To-Do List App</h1>
+      <p>Start building your tasks here!</p>
+    </div>
+  );
+}
